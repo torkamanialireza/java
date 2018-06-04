44537bda547874060cc975a07c511dddffd37d3b
AliReza <torkamani.alireza@gmail.com>
2018-06-04 13:16:51 -0700

    commited

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		int num = 100;
+		System.out.println("my name is Coding Dojo");
+		System.out.println("I am" + num + "years old");
+		System.out.println("My hometown is Burbank");
+
+
+	}
+
+}
